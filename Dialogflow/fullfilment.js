996574c606c6f643e6149cac4cebfd4235993d67
Niccolò Matteo Borgato <borgato.niccolo@gmail.com>
2023-06-21 15:28:41 +0200

    advancements

--- Dialogflow/fullfilment.js
+++ Dialogflow/fullfilment.js
@@ -15,20 +15,12 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
     console.log("Dialogflow Request body: " + JSON.stringify(request.body));
 
     function nutrition_info(agent) {
-      agent.context.set("intent_nutrition_info_question", 1);
-    }
-
-    function handleUserInput(agent) {
-      // Get user's input
-      const userInput = agent.query;
-
-      // Handle the user's input
-      agent.add(`Handling user's input: ${userInput}`);
+      const nutrition_question = agent.parameters["LLMQuestion"];
+      agent.add(`Handling user's input: ${nutrition_question}`);
     }
 
     let intentMap = new Map();
     intentMap.set("intent_nutrition_info", nutrition_info);
-    intentMap.set("intent_nutrition_info_question", handleUserInput); // Corrected event name
     agent.handleRequest(intentMap);
   }
 );
